2bc25ab6c3f4773a1bc7c645daea6098eaeb7097
do-year <rize84@naver.com>
2024-01-16 23:43:57 +0900

    #	modified:   src/components/Loading.js #	modified:   src/css/Loading.module.css

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,12 +1,7 @@
 import styles from "../css/Loading.module.css";
 
 function Loading() {
-  return (
-    <div className={styles["loading-container"]}>
-      <div className={styles.loading}></div>
-      <div id={styles["loading-text"]}>loading</div>
-    </div>
-  );
+  return <div className={styles.loader}></div>;
 }
 
 export default Loading;
